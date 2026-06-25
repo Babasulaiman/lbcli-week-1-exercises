ed088e7b6982e06e24186196fc62f52eeedcc6db
Babasulaiman <habeebbabasulaiman@gmail.com>
2026-06-25 23:07:01 +0100

    learning bitcoin from the command line

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" >/dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
